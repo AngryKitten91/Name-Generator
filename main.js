cb20d2144e08301c8158a2f835cbaf40c403156d
bart <bart@bart.com>
2018-09-01 21:10:52 +0200

    Initial Commit

---
+++ main.js
@@ -0,0 +1,14 @@
+var arrays = require("./arrays.js");
+
+function createName(a, b, num) {
+    function pickHelp(a){
+        return a[Math.floor((Math.random() * a.length))].toUpperCase();
+    }
+    for(let i = 0; i < num; i++){
+        console.error(`${pickHelp(a)} ${pickHelp(b)}`);
+        // console.log(pickHelp(a), pickHelp(b));
+    }
+}
+
+createName(arrays.adjective, arrays.noun, 1);
+
